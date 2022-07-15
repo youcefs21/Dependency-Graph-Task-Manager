13adc21c1a8fc46adf5db4a9324ca445b4502cae
youcef <youcefs2121@gmail.com>
2022-07-15 10:48:43 -0400

    don't refetch data on window focus

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -40,7 +40,7 @@ export default withTRPC<AppRouter>({
       /**
        * @link https://react-query.tanstack.com/reference/QueryClient
        */
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+      queryClientConfig: { defaultOptions: { queries: { refetchOnWindowFocus: false, staleTime: 60 } } },
     };
   },
   /**
