b8bc7a998e22496643c6ba90ebb729675d2f03f4
youcef <youcefs2121@gmail.com>
2022-09-18 19:07:16 -0400

    replace stroke-width with strokeWidth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
         <button onClick={() => setCollapseConfig((ex) => !ex)} className={"rounded-lg hover:bg-neutral-700 w-fit h-fit p-4 mx-4"}>
           <div className="w-[16px] h-[16px]">
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="8" cy="8" r="5" stroke="#D9D9D9" stroke-width="2"/>
+              <circle cx="8" cy="8" r="5" stroke="#D9D9D9" strokeWidth="2"/>
               <rect x="7" width="2" height="4" rx="1" fill="#D9D9D9"/>
               <rect y="9" width="2" height="4" rx="1" transform="rotate(-90 0 9)" fill="#D9D9D9"/>
               <rect x="12" y="9" width="2" height="4" rx="1" transform="rotate(-90 12 9)" fill="#D9D9D9"/>
