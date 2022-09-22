7824d3e246f694e2e863978ddaea8745b14d56f3
youcef <youcefs2121@gmail.com>
2022-09-22 11:59:16 -0400

    login button on demo page

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -49,22 +49,26 @@ export default function Component() {
 
         <Toolbar currentTool={currentTool} setCurrentTool={setCurrentTool} graph={graph}/>
 
-        <button onClick={() => setCollapseConfig((ex) => !ex)} className={"rounded-lg hover:bg-neutral-700 w-fit h-fit p-4 mx-4"}>
-          <div className="w-[16px] h-[16px]">
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="8" cy="8" r="5" stroke="#D9D9D9" strokeWidth="2"/>
-              <rect x="7" width="2" height="4" rx="1" fill="#D9D9D9"/>
-              <rect y="9" width="2" height="4" rx="1" transform="rotate(-90 0 9)" fill="#D9D9D9"/>
-              <rect x="12" y="9" width="2" height="4" rx="1" transform="rotate(-90 12 9)" fill="#D9D9D9"/>
-              <rect x="1" y="3.41418" width="2" height="3.4326" rx="1" transform="rotate(-45 1 3.41418)" fill="#D9D9D9"/>
-              <rect x="10.4932" y="11.9073" width="2" height="3.71686" rx="1" transform="rotate(-45 10.4932 11.9073)" fill="#D9D9D9"/>
-              <rect x="7" y="12" width="2" height="4" rx="1" fill="#D9D9D9"/>
-              <rect x="12.9694" y="1.39758" width="2" height="3.50127" rx="1" transform="rotate(45 12.9694 1.39758)" fill="#D9D9D9"/>
-              <rect x="3.62817" y="10" width="2" height="3.71686" rx="1" transform="rotate(45 3.62817 10)" fill="#D9D9D9"/>
-            </svg>
-          </div>
-        </button>
-
+        <div className="flex flex-wrap justify-center">
+          <a className="bg-gray-700 rounded my-1 p-2 text-white hover:bg-blue-500" href="/">
+            Login
+          </a>
+          <button onClick={() => setCollapseConfig((ex) => !ex)} className={"rounded-lg hover:bg-neutral-700 w-fit h-fit p-4 mx-4"}>
+            <div className="w-[16px] h-[16px]">
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="8" cy="8" r="5" stroke="#D9D9D9" strokeWidth="2"/>
+                <rect x="7" width="2" height="4" rx="1" fill="#D9D9D9"/>
+                <rect y="9" width="2" height="4" rx="1" transform="rotate(-90 0 9)" fill="#D9D9D9"/>
+                <rect x="12" y="9" width="2" height="4" rx="1" transform="rotate(-90 12 9)" fill="#D9D9D9"/>
+                <rect x="1" y="3.41418" width="2" height="3.4326" rx="1" transform="rotate(-45 1 3.41418)" fill="#D9D9D9"/>
+                <rect x="10.4932" y="11.9073" width="2" height="3.71686" rx="1" transform="rotate(-45 10.4932 11.9073)" fill="#D9D9D9"/>
+                <rect x="7" y="12" width="2" height="4" rx="1" fill="#D9D9D9"/>
+                <rect x="12.9694" y="1.39758" width="2" height="3.50127" rx="1" transform="rotate(45 12.9694 1.39758)" fill="#D9D9D9"/>
+                <rect x="3.62817" y="10" width="2" height="3.71686" rx="1" transform="rotate(45 3.62817 10)" fill="#D9D9D9"/>
+              </svg>
+            </div>
+          </button>
+        </div>
 
       </div>
 
