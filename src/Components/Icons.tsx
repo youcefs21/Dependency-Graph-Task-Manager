dd8668e1fec28c5587442d54c6a04c701e8f0b03
youcef <youcefs2121@gmail.com>
2022-09-27 20:48:58 -0400

    settings and burger menu icon components

---
+++ src/Components/Icons.tsx
@@ -0,0 +1,29 @@
+export const SettingsIcon = () => {
+  return (
+    <div className="w-[16px] h-[16px]">
+      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="8" cy="8" r="5" stroke="#D9D9D9" strokeWidth="2"/>
+        <rect x="7" width="2" height="4" rx="1" fill="#D9D9D9"/>
+        <rect y="9" width="2" height="4" rx="1" transform="rotate(-90 0 9)" fill="#D9D9D9"/>
+        <rect x="12" y="9" width="2" height="4" rx="1" transform="rotate(-90 12 9)" fill="#D9D9D9"/>
+        <rect x="1" y="3.41418" width="2" height="3.4326" rx="1" transform="rotate(-45 1 3.41418)" fill="#D9D9D9"/>
+        <rect x="10.4932" y="11.9073" width="2" height="3.71686" rx="1" transform="rotate(-45 10.4932 11.9073)" fill="#D9D9D9"/>
+        <rect x="7" y="12" width="2" height="4" rx="1" fill="#D9D9D9"/>
+        <rect x="12.9694" y="1.39758" width="2" height="3.50127" rx="1" transform="rotate(45 12.9694 1.39758)" fill="#D9D9D9"/>
+        <rect x="3.62817" y="10" width="2" height="3.71686" rx="1" transform="rotate(45 3.62817 10)" fill="#D9D9D9"/>
+      </svg>
+    </div>
+  )
+}
+
+export const BurgerMenuIcon = () => {
+  return (
+    <div className="w-[14px] h-[12px]">
+      <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="14" height="2" rx="1" fill="#D9D9D9"/>
+        <rect y="5" width="14" height="2" rx="1" fill="#D9D9D9"/>
+        <rect y="10" width="14" height="2" rx="1" fill="#D9D9D9"/>
+      </svg>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { GraphConfigPanel } from "../Components/Panels/GraphConfig";
 import { GroupConfigPanel, NodeConfigPanel } from "../Components/Panels/NodeConfig";
 import { TreeExplorerPanel } from "../Components/Panels/TreePanel";
 import { hintText, Toolbar, toolStates } from "../Components/Toolbar/Toolbar";
+import {BurgerMenuIcon, SettingsIcon} from "../Components/Icons";
 
 
 const Home: NextPage = () => {
@@ -49,32 +50,14 @@ const Home: NextPage = () => {
         <button onClick={() => {
           setCollapseExplorer((explorer) => !explorer)
         }} className="w-fit h-fit p-4 hover:bg-neutral-700 rounded-lg mx-4">
-          <div className="w-[14px] h-[12px]">
-            <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect width="14" height="2" rx="1" fill="#D9D9D9"/>
-              <rect y="5" width="14" height="2" rx="1" fill="#D9D9D9"/>
-              <rect y="10" width="14" height="2" rx="1" fill="#D9D9D9"/>
-            </svg>
-          </div>
+          <BurgerMenuIcon/>
         </button>
 
 
         <Toolbar currentTool={currentTool} setCurrentTool={setCurrentTool} graph={graph}/>
 
         <button onClick={() => setCollapseConfig((ex) => !ex)} className={"rounded-lg hover:bg-neutral-700 w-fit h-fit p-4 mx-4"}>
-          <div className="w-[16px] h-[16px]">
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="8" cy="8" r="5" stroke="#D9D9D9" strokeWidth="2"/>
-              <rect x="7" width="2" height="4" rx="1" fill="#D9D9D9"/>
-              <rect y="9" width="2" height="4" rx="1" transform="rotate(-90 0 9)" fill="#D9D9D9"/>
-              <rect x="12" y="9" width="2" height="4" rx="1" transform="rotate(-90 12 9)" fill="#D9D9D9"/>
-              <rect x="1" y="3.41418" width="2" height="3.4326" rx="1" transform="rotate(-45 1 3.41418)" fill="#D9D9D9"/>
-              <rect x="10.4932" y="11.9073" width="2" height="3.71686" rx="1" transform="rotate(-45 10.4932 11.9073)" fill="#D9D9D9"/>
-              <rect x="7" y="12" width="2" height="4" rx="1" fill="#D9D9D9"/>
-              <rect x="12.9694" y="1.39758" width="2" height="3.50127" rx="1" transform="rotate(45 12.9694 1.39758)" fill="#D9D9D9"/>
-              <rect x="3.62817" y="10" width="2" height="3.71686" rx="1" transform="rotate(45 3.62817 10)" fill="#D9D9D9"/>
-            </svg>
-          </div>
+          <SettingsIcon/>
         </button>
 
 
